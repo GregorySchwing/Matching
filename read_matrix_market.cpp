53fce2331f322fa9e4818198333fa21f86125b71
greg <go2432@wayne.edu>
2024-01-03 23:08:56 -0600

    push all verts on stack with nonz degr

--- read_matrix_market.cpp
+++ read_matrix_market.cpp
@@ -150,6 +150,12 @@ int main(int argc, char **argv) {
     // Tracks whether a vertex has been in the stack, and which edge is next.
     std::vector<int64_t> state(N,0);
     std::list<int64_t> K;
+    std::list<int64_t> umatched_stack;
+    // push all non-zero degree vertices onto stack.
+    for (int i = 0; i < (indptr.size()-2)/2; ++i){
+        if (indptr[i]!=indptr[i+1])
+            umatched_stack.push_back(i);
+    }
 
     return 0;
 }
