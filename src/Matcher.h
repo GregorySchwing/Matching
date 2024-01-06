c0dc2beb284c7f1222926f806cbe4fe615c2914e
greg <go2432@wayne.edu>
2024-01-06 12:11:18 -0600

    ok need to shrink

--- src/Matcher.h
+++ src/Matcher.h
@@ -14,21 +14,27 @@ public:
     template <typename IT, typename VT>
     static void match(const Graph<IT, VT>& graph, 
                     std::vector<IT>& matching,
-                    std::list<int64_t> &stack,
+                    std::list<IT> &stack,
                     //std::unordered_map<int64_t,Vertex<int64_t>> &vertexMap,
                     std::vector<Vertex<IT>> & vertexVector);
     template <typename IT, typename VT>
     static Vertex<IT> * search(const Graph<IT, VT>& graph, 
                     const std::vector<IT>& matching, 
                     const size_t V_index,
-                    std::list<int64_t> &stack,
+                    std::list<IT> &stack,
                     //std::unordered_map<int64_t,Vertex<int64_t>> &vertexMap,
                     std::vector<Vertex<IT>> & vertexVector);
+    template <typename IT, typename VT>
+    static void pushEdgesOntoStack(const Graph<IT, VT>& graph, 
+                                    std::vector<Vertex<IT>> & vertexVector, 
+                                    IT V_index, 
+                                    std::list<IT> &stack);
+
 };
 template <typename IT, typename VT>
 void Matcher::match(const Graph<IT, VT>& graph, 
                     std::vector<IT>& matching,
-                    std::list<int64_t> &stack,
+                    std::list<IT> &stack,
                     //std::unordered_map<int64_t,Vertex<int64_t>> &vertexMap,
                     std::vector<Vertex<IT>> & vertexVector) {
     // Access the graph elements as needed
@@ -43,48 +49,83 @@ template <typename IT, typename VT>
 Vertex<IT> * Matcher::search(const Graph<IT, VT>& graph, 
                     const std::vector<IT>& matching, 
                     const size_t V_index,
-                    std::list<int64_t> &stack,
+                    std::list<IT> &stack,
                     //std::unordered_map<int64_t,Vertex<int64_t>> &vertexMap,
                     std::vector<Vertex<IT>> & vertexVector) {
-    Vertex<int64_t> *FromBase,*ToBase, *CurrentVertex;
-    IT edge;
+    Vertex<int64_t> *FromBase,*ToBase, *nextVertex;
+    int64_t FromBaseVertexID,ToBaseVertexID;
+    IT stackEdge, matchedEdge;
     IT nextVertexIndex;
     IT time = 0;
     bool Found = false;
     //auto inserted = vertexMap.try_emplace(V_index,Vertex<IT>(time++,Label::EvenLabel));
-    CurrentVertex = &vertexVector[V_index];
-    CurrentVertex->AgeField=time++;
-    CurrentVertex->LabelField=Label::EvenLabel;
-    // Push edges onto stack, breaking if that edge is a solution.
-    for (IT start = graph.indptr[V_index]; start < graph.indptr[V_index+1]; ++start){
-        stack.push_back(graph.indices[start]);
-
-        nextVertexIndex = Edge<IT,VT>::Other(graph,graph.indices[start],V_index);
-       
-        CurrentVertex = &vertexVector[nextVertexIndex];
-        if (!CurrentVertex->IsReached() && !CurrentVertex->IsMatched())
-            break;
-    }
+    nextVertex = &vertexVector[V_index];
+    nextVertex->AgeField=time++;
+    nextVertex->LabelField=Label::EvenLabel;
+    // Push edges onto stack, breaking if that stackEdge is a solution.
+    pushEdgesOntoStack<IT,VT>(graph,vertexVector,V_index,stack);
     while(!stack.empty()){
-        edge = stack.back();
+        stackEdge = stack.back();
         stack.pop_back();
-        FromBase = Blossom<IT>::Base(&vertexVector[Edge<IT,VT>::EdgeFrom(graph,edge)]);
-        ToBase = Blossom<IT>::Base(&vertexVector[Edge<IT,VT>::EdgeTo(graph,edge)]);
+        // Necessary because vertices dont know their own index.
+        // It simplifies vector creation..
+        FromBaseVertexID = Edge<IT,VT>::EdgeFrom(graph,stackEdge);
+        FromBase = Blossom<IT>::Base(&vertexVector[FromBaseVertexID]);
+        // Necessary because vertices dont know their own index.
+        // It simplifies vector creation..
+        ToBaseVertexID = Edge<IT,VT>::EdgeTo(graph,stackEdge);
+        ToBase = Blossom<IT>::Base(&vertexVector[ToBaseVertexID]);
         // Edge is between two vertices in the same blossom, continue.
         if (FromBase == ToBase)
             continue;
-        if(!FromBase->IsEven())
+        if(!FromBase->IsEven()){
             std::swap(FromBase,ToBase);
+            std::swap(FromBaseVertexID,ToBaseVertexID);
+        }
         // An unreached, unmatched vertex is found, AN AUGMENTING PATH!
         if (!ToBase->IsReached() && !ToBase->IsMatched()){
             ToBase->LabelField=Label::OddLabel;
-            ToBase->TreeField=edge;
+            ToBase->TreeField=stackEdge;
             ToBase->AgeField=time++;
-            ToBase->MatchField=edge;
+            ToBase->MatchField=stackEdge;
             // I'll let the augment path method recover the path.
             return ToBase;
+        } else if (!ToBase->IsReached() && ToBase->IsMatched()){
+            ToBase->LabelField=Label::OddLabel;
+            ToBase->TreeField=stackEdge;
+            ToBase->AgeField=time++;
+
+            matchedEdge=ToBase->MatchField;
+            nextVertexIndex = Edge<IT,VT>::Other(graph,matchedEdge,ToBaseVertexID);
+            nextVertex = &vertexVector[nextVertexIndex];
+            nextVertex->LabelField=Label::EvenLabel;
+            nextVertex->AgeField=time++;
+            pushEdgesOntoStack<IT,VT>(graph,vertexVector,nextVertexIndex,stack);
+
+        } else {
+            // Shrink Blossoms
         }
     }
     return nullptr;
+}
+
+template <typename IT, typename VT>
+void Matcher::pushEdgesOntoStack(const Graph<IT, VT>& graph, 
+                                    std::vector<Vertex<IT>> & vertexVector, 
+                                    IT V_index, 
+                                    std::list<IT> &stack){
+    IT nextVertexIndex;
+    Vertex<IT>* nextVertex;
+
+    // Push edges onto stack, breaking if that stackEdge is a solution.
+    for (IT start = graph.indptr[V_index]; start < graph.indptr[V_index + 1]; ++start) {
+        stack.push_back(graph.indices[start]);
+
+        nextVertexIndex = Edge<IT, VT>::Other(graph, graph.indices[start], V_index);
+
+        nextVertex = &vertexVector[nextVertexIndex];
+        if (!nextVertex->IsReached() && !nextVertex->IsMatched())
+            break;
+    }
 }
 #endif
