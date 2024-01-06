699d9ec13fd176d41a2ed06d85598cc18adb03d6
greg <go2432@wayne.edu>
2024-01-06 11:41:52 -0600

    vector seems quick enough

--- Main.cpp
+++ Main.cpp
@@ -20,10 +20,26 @@ int main(int argc, char **argv) {
     std::filesystem::path in_path{argv[1]};
     double start_time_csc_2_g, end_time_csc_2_g;
     Graph<int64_t, std::string>  G(in_path);
+
     std::vector<int64_t> matching;
+    // A map is used for the frontier to limit copying N vertices.
+    //std::unordered_map<int64_t, Vertex<int64_t>> vertexMap;
+    // A vector is used for the frontier to allocate once all the memory ever needed.
+    std::vector<Vertex<int64_t>> vertexVector;
+    // std::list is used for its splicing efficiency
+    // Transfers elements from one list to another. 
+    // No elements are copied or moved, only the internal 
+    // pointers of the list nodes are re-pointed.
+    // https://en.cppreference.com/w/cpp/container/list/splice
+    std::list<int64_t> stack;
+    auto allocate_start = high_resolution_clock::now();
     matching.resize(G.getN(),-1);
+    vertexVector.resize(G.getN());
+    auto allocate_end = high_resolution_clock::now();
+    auto duration_alloc = duration_cast<milliseconds>(allocate_end - allocate_start);
+    std::cout << "|V|-vector memory allocation time: "<< duration_alloc.count() << " milliseconds" << std::endl;
     auto match_start = high_resolution_clock::now();
-    Matcher::match<int64_t, std::string>(G,matching);
+    Matcher::match<int64_t, std::string>(G,matching,stack,vertexVector);
     auto match_end = high_resolution_clock::now();
     auto duration = duration_cast<seconds>(match_end - match_start);
     std::cout << "Maximum matching time: "<< duration.count() << " seconds" << std::endl;
--- src/Matcher.h
+++ src/Matcher.h
@@ -12,49 +12,68 @@
 class Matcher {
 public:
     template <typename IT, typename VT>
-    static void match(const Graph<IT, VT>& graph, std::vector<IT>& matching);
+    static void match(const Graph<IT, VT>& graph, 
+                    std::vector<IT>& matching,
+                    std::list<int64_t> &stack,
+                    //std::unordered_map<int64_t,Vertex<int64_t>> &vertexMap,
+                    std::vector<Vertex<IT>> & vertexVector);
     template <typename IT, typename VT>
-    static bool search(const Graph<IT, VT>& graph, const std::vector<IT>& matching, const size_t V_index);
+    static bool search(const Graph<IT, VT>& graph, 
+                    const std::vector<IT>& matching, 
+                    const size_t V_index,
+                    std::list<int64_t> &stack,
+                    //std::unordered_map<int64_t,Vertex<int64_t>> &vertexMap,
+                    std::vector<Vertex<IT>> & vertexVector);
 };
 template <typename IT, typename VT>
-void Matcher::match(const Graph<IT, VT>& graph, std::vector<IT>& matching) {
+void Matcher::match(const Graph<IT, VT>& graph, 
+                    std::vector<IT>& matching,
+                    std::list<int64_t> &stack,
+                    //std::unordered_map<int64_t,Vertex<int64_t>> &vertexMap,
+                    std::vector<Vertex<IT>> & vertexVector) {
     // Access the graph elements as needed
     for (std::size_t i = 0; i < graph.getN(); ++i) {
         if (matching[i] < 0) {
             // Your matching logic goes here...
-            search(graph,matching,i);
+            search(graph,matching,i,stack,vertexVector);
         }
     }
 }
 template <typename IT, typename VT>
-bool Matcher::search(const Graph<IT, VT>& graph, const std::vector<IT>& matching, const size_t V_index) {
-    // std::list is used for its splicing efficiency
-    // Transfers elements from one list to another. 
-    // No elements are copied or moved, only the internal 
-    // pointers of the list nodes are re-pointed.
-    // https://en.cppreference.com/w/cpp/container/list/splice
-    std::list<IT> stack;
-    // A map is used for the frontier to limit copying N vertices.
-    std::unordered_map<IT, Vertex<IT>> vertexMap;
+bool Matcher::search(const Graph<IT, VT>& graph, 
+                    const std::vector<IT>& matching, 
+                    const size_t V_index,
+                    std::list<int64_t> &stack,
+                    //std::unordered_map<int64_t,Vertex<int64_t>> &vertexMap,
+                    std::vector<Vertex<IT>> & vertexVector) {
+    Vertex<int64_t> *FromBase,*ToBase, *CurrentVertex;
     IT edge;
     IT nextVertexIndex;
-    Vertex<IT> nextVertex;
-    Vertex<IT> *FromBase,*ToBase;
     IT time = 0;
     bool Found = false;
-    auto inserted = vertexMap.try_emplace(V_index,Vertex<IT>(time++,Label::EvenLabel));
-
+    //auto inserted = vertexMap.try_emplace(V_index,Vertex<IT>(time++,Label::EvenLabel));
+    CurrentVertex = &vertexVector[V_index];
+    CurrentVertex->AgeField=time++;
+    CurrentVertex->LabelField=Label::EvenLabel;
     // Push edges onto stack, breaking if that edge is a solution.
     for (IT start = graph.indptr[V_index]; start < graph.indptr[V_index+1]; ++start){
         stack.push_back(graph.indices[start]);
+
         nextVertexIndex = Edge<IT,VT>::Other(graph,graph.indices[start],V_index);
+       
+        CurrentVertex = &vertexVector[nextVertexIndex];
+        if (!CurrentVertex->IsReached() && !CurrentVertex->IsMatched())
+            break;
+        /*
         auto inserted = vertexMap.try_emplace(nextVertexIndex,Vertex<IT>{});
         if (!inserted.first->second.IsReached() && !inserted.first->second.IsMatched())
             break;
+        */
     }
     while(!stack.empty()){
         edge = stack.back();
         stack.pop_back();
+        /*
         auto inserted = vertexMap.try_emplace(Edge<IT,VT>::EdgeTo(graph,edge),Vertex<IT>{});
         // Successfully inserted new object, need to update ParentField to *this pointer
         if (inserted.second)
@@ -66,6 +85,24 @@ bool Matcher::search(const Graph<IT, VT>& graph, const std::vector<IT>& matching
 
         FromBase = Blossom<IT>::Base(&inserted.first->second);
         ToBase = Blossom<IT>::Base(&inserted2.first->second);
+        // Edge is between two vertices in the same blossom, continue.
+        if (FromBase == ToBase)
+            continue;
+        if(!FromBase->IsEven())
+            std::swap(FromBase,ToBase);
+        // An unreached, unmatched vertex is found, AN AUGMENTING PATH!
+        if (0 && !ToBase->IsReached() && !ToBase->IsMatched()){
+            ToBase->LabelField=Label::OddLabel;
+            ToBase->TreeField=edge;
+            ToBase->AgeField=time++;
+            // TODO RECOVER(ToBase)
+            ToBase->MatchField=edge;
+            Found = true;
+            break;
+        }
+        */
+        FromBase = Blossom<IT>::Base(&vertexVector[Edge<IT,VT>::EdgeFrom(graph,edge)]);
+        ToBase = Blossom<IT>::Base(&vertexVector[Edge<IT,VT>::EdgeTo(graph,edge)]);
         // Edge is between two vertices in the same blossom, continue.
         if (FromBase == ToBase)
             continue;
