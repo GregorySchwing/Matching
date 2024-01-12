91bc8f31577e15284229f165a749a959f6634f2e
greg <go2432@wayne.edu>
2024-01-12 15:45:41 -0600

    Revert "buggy"
    This reverts commit db9b00df6ce2935e5dbe4126ffe7f2b1cd7080dd.

--- src/Matcher.h
+++ src/Matcher.h
@@ -11,15 +11,11 @@
 #include "Stack.h"
 #include "Frontier.h"
 #include "Statistics.h"
-#include "SPMCQueue.h"
-#include "SPMC_Config.h"
 
 class Matcher {
 public:
     template <typename IT, typename VT>
     static void match(Graph<IT, VT>& graph);
-    template <typename IT, typename VT>
-    static void match_parallel(Graph<IT, VT>& graph);
     template <typename IT, typename VT>
     static void match(Graph<IT, VT>& graph, Statistics<IT>& stats);
 
@@ -28,12 +24,6 @@ private:
     static Vertex<IT> * search(Graph<IT, VT>& graph, 
                     const size_t V_index,
                     Frontier<IT> & f);
-    template <typename IT, typename VT, typename Q>
-    static void search_slave(Graph<IT, VT>& graph, 
-                        std::atomic<bool>& atomic_flag, // Make flag atomic
-                        std::atomic<IT>& atomic_numActive, // Make numActive atomic
-                        Q& q,
-                        int tid, int cpu); // For record-keeping and CPU binding
     template <typename IT, typename VT>
     static void augment(Graph<IT, VT>& graph, 
                     Vertex<IT> * TailOfAugmentingPath,
@@ -73,64 +63,6 @@ void Matcher::match(Graph<IT, VT>& graph) {
                 //printf("DIDNT FOUND AP!\n");
             }
         }
-    }
-}
-
-
-template <typename IT, typename VT>
-void Matcher::match_parallel(Graph<IT, VT>& graph) {
-
-    using Q = SPMCQueue<Msg, 1024>;
-    Q q;
-    std::atomic<bool> atomic_flag = true; // Make flag atomic
-    std::atomic<IT> atomic_numActive = 0; // Make numActive atomic
-
-    int reader_cnt = 2;
-    int cpu_start = 0;
-    std::vector<thread> reader_thrs;
-    for (int i = 0; i < reader_cnt; i++) {
-        //reader_thrs.emplace_back(read_thread, i, cpu_start < 0 ? -1 : cpu_start + i, std::ref(q));
-        reader_thrs.emplace_back(std::ref(Matcher::search_slave<IT, VT, Q>), std::ref(graph), std::ref(atomic_flag), std::ref(atomic_numActive), std::ref(q), i, cpu_start + i);
-
-    }
-
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(1));
-
-    if (cpu_start >= 0) {
-        cpupin(cpu_start + reader_cnt);
-    }
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(1));
-    std::cout << "WR THIS :" << &q << std::endl;
-
-    // Access the graph elements as needed
-    for (std::size_t i = 0; i < graph.getN(); ++i) {
-        if (graph.matching[i] < 0) {
-            IT V_ndx = i;
-            // Push edges onto stack, breaking if that stackEdge is a solution.
-            for (IT start = graph.indptr[V_ndx]; start < graph.indptr[V_ndx + 1]; ++start) {
-                // For blossom contraction, need to skip repushing the matched & tree edges
-                IT E_ndx = graph.indices[start];
-                
-                q.write([V_ndx,E_ndx](Msg& msg) {
-                    msg.V_ndx = V_ndx;
-                    msg.E_ndx = E_ndx;
-                    msg.tsc = rdtsc();
-                });
-                
-                std::cout << "Writing V_ndx: "<<V_ndx<<"; E_ndx: " << E_ndx << std::endl;
-                IT nextVertexIndex = Graph<IT, VT>::Other(graph, graph.indices[start], V_ndx);
-                if (!graph.IsMatched(nextVertexIndex))
-                    break;
-            }
-            while(true){}
-
-        }
-    }
-
-    for (auto& thr : reader_thrs) {
-        thr.join();
     }
 }
 
@@ -236,94 +168,6 @@ Vertex<IT> * Matcher::search(Graph<IT, VT>& graph,
     }
     return nullptr;
 }
-
-
-template <typename IT, typename VT, typename Q>
-void Matcher::search_slave(Graph<IT, VT>& graph, 
-                    std::atomic<bool>& atomic_flag, // Make flag atomic
-                    std::atomic<IT>& atomic_numActive, // Make numActive atomic
-                    Q& q,
-                    int tid, int cpu) {
-    if (cpu >= 0) {
-        cpupin(cpu);
-    }
-    auto reader = q.getReader();
-    Frontier<IT> f(graph.getN(),graph.getM());
-    Vertex<int64_t> *FromBase,*ToBase, *nextVertex;
-    int64_t FromBaseVertexID,ToBaseVertexID;
-    IT stackEdge, matchedEdge;
-    IT nextVertexIndex;
-    Stack<IT> &stack = f.stack;
-    Stack<IT> &tree = f.tree;
-    DisjointSetUnion<IT> &dsu = f.dsu;
-    std::vector<Vertex<IT>> & vertexVector = f.vertexVector;
-    while(true){
-        Msg* msg = reader.read();
-        if (!msg){
-            //std::cout << "Thread " << tid << " failed to read!\n" << std::endl;
-            continue;
-        } else {
-            std::cout << "Thread " << tid << " read!\n" << std::endl;
-        }
-        atomic_numActive++;
-        IT V_index;// = msg->V_ndx;
-        IT E_index;// = msg->E_ndx;
-        nextVertex = &vertexVector[V_index];
-        tree.push_back(V_index);
-        IT time = 0;
-        nextVertex->AgeField=time++;
-        // Push edges onto stack, breaking if that stackEdge is a solution.
-        stack.push_back(E_index);
-        while(atomic_flag.load() && !stack.empty()){
-            stackEdge = stack.back();
-            stack.pop_back();
-            // Necessary because vertices dont know their own index.
-            // It simplifies vector creation..
-            FromBaseVertexID = dsu[Graph<IT,VT>::EdgeFrom(graph,stackEdge)];
-            FromBase = &vertexVector[FromBaseVertexID];
-
-            // Necessary because vertices dont know their own index.
-            // It simplifies vector creation..
-            ToBaseVertexID = dsu[Graph<IT,VT>::EdgeTo(graph,stackEdge)];
-            ToBase = &vertexVector[ToBaseVertexID];
-
-            // Edge is between two vertices in the same blossom, continue.
-            if (FromBase == ToBase)
-                continue;
-            if(!FromBase->IsEven()){
-                std::swap(FromBase,ToBase);
-                std::swap(FromBaseVertexID,ToBaseVertexID);
-            }
-            // An unreached, unmatched vertex is found, AN AUGMENTING PATH!
-            if (!ToBase->IsReached() && !graph.IsMatched(ToBaseVertexID)){
-                ToBase->TreeField=stackEdge;
-                ToBase->AgeField=time++;
-                tree.push_back(ToBaseVertexID);
-                //graph.SetMatchField(ToBaseVertexID,stackEdge);
-                // I'll let the augment path method recover the path.
-                //return ToBase;
-            } else if (!ToBase->IsReached() && graph.IsMatched(ToBaseVertexID)){
-                ToBase->TreeField=stackEdge;
-                ToBase->AgeField=time++;
-                tree.push_back(ToBaseVertexID);
-
-                matchedEdge=graph.GetMatchField(ToBaseVertexID);
-                nextVertexIndex = Graph<IT,VT>::Other(graph,matchedEdge,ToBaseVertexID);
-                nextVertex = &vertexVector[nextVertexIndex];
-                nextVertex->AgeField=time++;
-                tree.push_back(nextVertexIndex);
-
-                Graph<IT,VT>::pushEdgesOntoStack(graph,vertexVector,nextVertexIndex,stack,matchedEdge);
-
-            } else if (ToBase->IsEven()) {
-                // Shrink Blossoms
-                // Not sure if this is wrong or the augment method is wrong
-                Blossom::Shrink(graph,stackEdge,dsu,vertexVector,stack);
-            }
-        }
-    }
-}
-
 
 template <typename IT, typename VT>
 void Matcher::augment(Graph<IT, VT>& graph, 
