630a6deedb1701ababb2233d4b55415ba6b6819c
go2432 <go2432@wayne.edu>
2024-01-29 15:22:30 -0500

    add rmat

---
+++ graphs/RMAT/run_cpu_potoff.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --job-name wl 
+
+#SBATCH -N 1
+
+#SBATCH -n 16 
+
+#SBATCH --mem=24G
+#SBATCH --nodelist=ressrv6ai8111
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=go2432@wayne.edu
+
+#SBATCH -o output_%j.out
+
+#SBATCH -e errors_%j.err
+
+#SBATCH -t 7-0:0:0
+#eval "$(conda shell.bash hook)"
+echo $HOSTNAME
+bash ./test.sh
+#bash ../graphs/dimacs/run_cpu.txt
